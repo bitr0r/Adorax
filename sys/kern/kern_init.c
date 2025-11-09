58ccdd29b4f9859243865385b4cc790a7a5ed9fe
Ian Moffett <ian@osmora.org>
2025-11-09 16:15:59 -0500

    kern: Add initial kernel entry
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ sys/kern/kern_init.c
@@ -0,0 +1,16 @@
+/*
+ * Kernel entrypoint
+ * Copyright (c) 2025, Ian Moffett and the Adorax team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#include <md/mb.h>
+
+void kentry(void);
+
+void
+kentry(void)
+{
+    for (;;);
+}
