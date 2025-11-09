0e995067d7a0b6a72c3f1fa08ef3651e54f27eee
Ian Moffett <ian@osmora.org>
2025-11-09 16:52:35 -0500

    sys: Add initial system types
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ sys/include/sys/types.h
@@ -0,0 +1,23 @@
+/*
+ * System types
+ * Copyright (c) 2025, Ian Moffett and the Adorax team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#ifndef _SYS_TYPES_H_
+#define _SYS_TYPES_H_ 1
+
+/* Unsigned types */
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
+
+/* Signed types */
+typedef char       int8_t;
+typedef short      int16_t;
+typedef int        int32_t;
+typedef long long  int64_t;
+
+#endif  /* !_SYS_TYPES_H_ */
